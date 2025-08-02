0e2ac887a4f4246f34c9a210c20414078edc5c5a
iMickey10 <bluetooth.mmn10@gmail.com>
2025-08-01 19:51:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); // <--- AÑADIDO
+
 const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 
 const app = express();
+app.use(cors()); // <--- AÑADIDO
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
